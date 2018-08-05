52450630e4ed969bba9dedeac306b4941be42bc9
manojha18 <mn258342@dal.ca>
2018-08-04 21:17:29 -0300

    Cleaned the dataset

--- Exploring the dataset.py
+++ Exploring the dataset.py
@@ -6,5 +6,9 @@ import seaborn as sns
 
 data = pd.read_csv('E:/Summer 18/Data management warehousing and analytics/Project/911.csv')
 print(data.head())
-
 print(data.info())
+
+# Removing unnecessary columns
+data = data.drop('e', axis=1)
+print(data.head(2))
+
