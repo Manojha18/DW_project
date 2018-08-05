f464e3c1012fad054faef8586bfcc20a8483aaf4
manojha18 <mn258342@dal.ca>
2018-08-04 22:19:33 -0300

    Extracting features from the attributes

--- Exploring the dataset.py
+++ Exploring the dataset.py
@@ -31,12 +31,44 @@ print(top_twp_5.head())
 top_5_plot = plt.figure(figsize=(5,5))
 sns.barplot(x='ZIP', y='Total_Count', data=top_zip_5)
 top_5_plot.tight_layout()
-plt.show()
+# plt.show()
 
 top_twp_plot = plt.figure(figsize=(5,5))
-label_twp=sns.barplot(x='TWP', y='Total_count', data= top_twp_5, palette="viridis")
-label_twp.set_xticklabels(label_twp.get_xticklabels(),rotation=90)
-plt.show()
+label_twp = sns.barplot(x='TWP', y='Total_count', data=top_twp_5, palette="viridis")
+label_twp.set_xticklabels(label_twp.get_xticklabels(), rotation=90)
+# plt.show()
+
+# EXTRACTING THE FEATURES FROM THE COLUMNS
+data['label'] = data['title'].str.split(':').str[0] #own
+data['label']
+print(data.head(2))
+
+print(data['label'].nunique())
+data['STA'] = data['desc'].str.split(';').str[2] #own
+print(data['STA'].head(2))
+
+print(data['label'].value_counts())
+# Splitting hour,days of week, month and date from timeStamp
+data['Hour'] = data.timeStamp.map(lambda x: pd.to_datetime(x).hour) #own
+data['Days'] = data.timeStamp.map(lambda x: pd.to_datetime(x).dayofweek) #own
+data['Month'] = data.timeStamp.map(lambda x: pd.to_datetime(x).month) #own
+
+
+# Converting day values to strings
+days = {0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday'}
+
+data['Days'] = data['Days'].map(days)
+print(data['Hour'].head(2))
+print(data.head(3))
+
+
+
 
 
 
