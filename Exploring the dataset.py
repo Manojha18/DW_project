0ecccf50f99e69aecee3fe0f0868dc04c5bc91f4
manojha18 <mn258342@dal.ca>
2018-08-04 21:12:07 -0300

    checking with the commit

--- Exploring the dataset.py
+++ Exploring the dataset.py
@@ -7,7 +7,4 @@ import seaborn as sns
 data = pd.read_csv('E:/Summer 18/Data management warehousing and analytics/Project/911.csv')
 print(data.head())
 
-top_10_zip = pd.DataFrame(data['zip'].value_counts().head(10))
-top_10_zip.reset_index(inplace=True)
-top_10_zip.columns = ['ZIP', 'Count']
-print(top_10_zip)
+print(data.info())
